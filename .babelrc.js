0b398a3060e19d93cc39b961d8245baa5b5bcf9a
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-07-12 16:41:28 +1000

    setup emotion

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@emotion/babel-plugin',
+    ['babel-plugin-twin', { debug: false }],
+    'babel-plugin-macros',
+  ],
+}
---
+++ babel-plugin-macros.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twin: {
+    preset: 'emotion',
+    autoCssProp: false,
+    dataTwProp: true,
+  },
+}
